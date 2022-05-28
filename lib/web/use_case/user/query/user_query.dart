3c9d64fe109cfad05738b05743744cbb8753c811
Lucas Jehanno <51371800+philoXof@users.noreply.github.com>
2022-05-29 00:24:06 +0200

    :art: some modification

--- lib/web/use_case/user/query/user_query.dart
+++ lib/web/use_case/user/query/user_query.dart
@@ -4,14 +4,17 @@ import 'package:http/http.dart' as http;
 import '../../../global.dart';
 import '../entities/user_response.dart';
 
-var global = new Global();
+class UserQuery {
+  var global = new Global();
 
-Future<UserResponse> getAll() async{
-  final response = await http
-      .get(Uri.parse(global.url + 'user/'));
-  if(response.statusCode == 200){
-    return UserResponse.fromJson(jsonDecode(response.body));
-  }else{
-    throw Exception('Failed to load users');
+  Future<UserResponse> getById(int id) async {
+    final response = await http.get(Uri.parse(global.buildUrl('user/' + id.toString())));
+    if (response.statusCode == 200) {
+      return UserResponse.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load users');
+    }
   }
+
+
 }
