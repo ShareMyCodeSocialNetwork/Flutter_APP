d1b3f8fc09125fe6ca2a02d02db7919a9466d73e
Lucas Jehanno <51371800+philoXof@users.noreply.github.com>
2022-05-30 10:32:30 +0200

    :sparkles: email exist (for register)

--- lib/web/use_case/user/query/user_query.dart
+++ lib/web/use_case/user/query/user_query.dart
@@ -1,29 +1,31 @@
 import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import '../../../global.dart';
+import '../../../global.dart' as global;
 import '../entities/user_response.dart';
 
-class UserQuery {
-  var global = new Global();
-
-  Future<UserResponse> getById(int id) async {
-    final response = await http.get(Uri.parse(global.buildUrl('user/' + id.toString())));
-    if (response.statusCode == 200) {
-      return UserResponse.fromJson(jsonDecode(response.body));
-    } else {
-      throw Exception('Failed to load users');
-    }
+Future<UserResponse> getById(int id) async {
+  final response =
+      await http.get(Uri.parse(global.buildUrl('user/' + id.toString())));
+  if (response.statusCode == 200) {
+    return UserResponse.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('Failed to load users');
   }
+}
 
-  Future<bool> pseudoExist(String pseudo) async {
-    final response = await http.get(Uri.parse(global.buildUrl('user/pseudo/' + pseudo)));
-    return response.statusCode == 200 ? true : false;
-  }
+Future<bool> pseudoExist(String pseudo) async {
+  final response = await http.get(Uri.parse(global.buildUrl('user/pseudo/' + pseudo)));
+  return response.statusCode == 200 ? true : false;
+}
 
-  Future<bool> userExist(int id) async {
-    final response = await http.get(Uri.parse(global.buildUrl('user/' + id.toString())));
-    return response.statusCode == 200 ? true : false;
-  }
+Future<bool> emailExist(String email) async{
+  final response = await http.get(Uri.parse(global.buildUrl('user/email/' + email)));
+  return response.statusCode == 200 ? true : false;
+}
 
+Future<bool> userExist(int id) async {
+  final response =
+      await http.get(Uri.parse(global.buildUrl('user/' + id.toString())));
+  return response.statusCode == 200 ? true : false;
 }
