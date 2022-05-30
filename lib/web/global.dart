f5659c1acab1e0144c0f1a0b1019c061763278ef
Lucas Jehanno <51371800+philoXof@users.noreply.github.com>
2022-05-30 10:34:12 +0200

    :rocket: global

--- lib/web/global.dart
+++ lib/web/global.dart
@@ -1,7 +1,16 @@
-class Global {
-  final String url = 'http://localhost:8080/';
+import 'package:flutter_app/web/use_case/user/entities/user_response.dart';
+import 'package:flutter/material.dart';
 
-  String buildUrl(String endUrl) {
-    return url + endUrl;
-  }
+final String url = 'http://localhost:8080/';
+String token = '';
+bool isConnected = false;
+String buildUrl(String endUrl) {
+  return url + endUrl;
 }
+
+UserResponse connectedUser = new UserResponse();
+
+String access_token = '';
+String refresh_token = '';
+
+Color buttonColor = Color.fromRGBO(36, 140, 69, 1);
